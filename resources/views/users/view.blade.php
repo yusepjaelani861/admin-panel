9978c5e51b67c8d2d52092cf6944782ecb7baf95
yusepjaelani861 <yusepjaelani861@gmail.com>
2023-05-04 13:43:20 +0700

    fix bug image

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -195,7 +195,7 @@ function formatPrice($price)
                                 <td class="border px-4 py-2">{{ $file->id }}</td>
                                 <td class="border px-4 py-2">{{ $file->folders ? $file->folders->name : 'None' }}</td>
                                 <td class="border px-4 py-2 flex flex-col items-center justify-center">
-                                    <img :src="$file - > original_url" :alt="$file - > name"
+                                    <img src="{{$file->original_url }}" alt="{{ $file->name }}"
                                         class="w-20 h-20 object-cover" />
                                     <p>{{ $file->name }}</p>
                                 </td>
